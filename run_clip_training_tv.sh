0028f1a422d173fec8312489ddf29f9b33e5c7b2
arampacha <aruthart@gmail.com>
2021-07-15 15:48:34 +0000

    training config upd

--- run_clip_training_tv.sh
+++ run_clip_training_tv.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 ./run_clip_flax_tv.py \
-    --output_dir /home/shared/models/clip-rsicd \
+    --output_dir /home/shared/models/clip-rsicd/bs128x8-lr1e-4-augs \
     --model_name_or_path openai/clip-vit-base-patch32 \
     --dataset_name $HOME/CLIP-rsicd/rsicd.py \
     --data_dir /home/shared/data \
@@ -11,24 +11,24 @@
     --per_device_train_batch_size="128" \
     --per_device_eval_batch_size="128" \
     --preprocessing_num_workers="16" \
-    --learning_rate="3e-4" \
+    --learning_rate="1e-4" \
     --adafactor \
-    --warmup_steps="20" \
+    --warmup_steps="50" \
     --adam_beta1="0.9" \
     --adam_beta2="0.98" \
     --weight_decay="0.1" \
     --overwrite_output_dir \
-    --num_train_epochs 5 \
+    --num_train_epochs 10 \
     --logging_steps="2" \
     --eval_steps="20" \
     --push_to_hub="False" \
     --dtype="bfloat16" \
     --skip_memory_metrics="True" \
     --save_steps="200" \
-    --save_total_limit 2 \
+    --save_total_limit 10 \
     --gradient_accumulation_steps 1 \
     --report_to all \
-    --run_name="testing-5-captions+img-augs" \
+    --run_name="bs128x8-lr1e-4-augs" \
     --save_strategy epoch \
     --save_optimizer="False" \
     --captions_per_image 5 \
